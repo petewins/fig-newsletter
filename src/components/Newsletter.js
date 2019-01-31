97d7e6b884c11f9e30a153004d7221006c281c86
Peter Liang <peter_liang@glic.com>
2019-01-31 17:52:02 -0500

    inital create-react-app

---
+++ src/components/Newsletter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Newsletter extends Component {
+  render() {
+    return (
+      <div className="newsletter">
+        <h1>Join the List!</h1>
+      </div>
+    );
+  }
+}
+
+export default Newsletter;
